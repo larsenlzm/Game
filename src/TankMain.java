d6f7079d6108dd3b77f90e1a5910187449c9381b
Elias Larsen <larsenlzm@dhcp-10-253-0-214.wlan.hioa.no>
2018-03-23 09:47:22 +0100

    bevegelsefixKappa

--- src/TankMain.java
+++ src/TankMain.java
@@ -11,13 +11,14 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
-import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
 import java.util.ArrayList;
+import java.util.BitSet;
 import java.util.List;
 
 public class TankMain extends Application {
@@ -29,6 +30,22 @@ public class TankMain extends Application {
 
     private GameObjects player;
     private GameObjects enemy;
+
+    public BitSet keyboardBitSet = new BitSet();
+
+    private void addInputControls(Scene scene) {
+        scene.addEventFilter(KeyEvent.KEY_PRESSED, event -> {
+            keyboardBitSet.set(event.getCode().ordinal(), true);
+        });
+        scene.addEventFilter(KeyEvent.KEY_RELEASED, event -> {
+            keyboardBitSet.set(event.getCode().ordinal(), false);
+        });
+    }
+
+    private void addBullet(GameObjects bullet, double x, double y) {
+        bullets.add(bullet);
+        addGameObject(bullet,x,y);
+    }
 
     private Parent createContent() {
 
@@ -55,54 +72,21 @@ public class TankMain extends Application {
         return root;
     }
 
-    private void addBullet(GameObjects bullet, double x, double y) {
-        bullets.add(bullet);
-        addGameObject(bullet,x,y);
-    }
-
-
-
     private void addGameObject(GameObjects object, double x, double y) {
         object.getView().setTranslateX(x);
         object.getView().setTranslateY(y);
         root.getChildren().add(object.getView());
     }
 
-    private void onUpdate() {
-        for (GameObjects bullet : bullets) {
-                if (bullet.isColliding(enemy)) {
-                    bullet.setAlive(false);
-                    enemy.setAlive(false);
-
-                    //remover bullet og enemy når de blir hit av bullet
-                    
-                }
-
-        }
-
-        bullets.removeIf(GameObjects::isDead);
-
-
-        bullets.forEach(GameObjects::update);
-
-
-
-        player.update();
-        enemy.update();
-        //Slett denne for å fjerne Røde dotter
-
-    }
-
-
     private static class Player extends GameObjects {
         Player() {
-            super(new ImageView("/anansi.png"));
+            super(new ImageView("/tank1.png"));
         }
     }
 
     private static class Enemy extends GameObjects {
         Enemy() {
-            super(new ImageView("/lul.png"));
+            super(new ImageView("/tank2.png"));
         }
     }
 
@@ -116,24 +100,40 @@ public class TankMain extends Application {
     public void start(Stage stage) throws Exception {
 
         stage.setScene(new Scene(createContent()));
-        stage.getScene().setOnKeyPressed(e -> {
-            if (e.getCode() == KeyCode.LEFT) {
+
+        addInputControls(stage.getScene());
+
+
+        stage.show();
+
+    }
+
+    private void onUpdate() {
+
+
+        boolean isAPressed = keyboardBitSet.get(KeyCode.A.ordinal());
+        boolean isDPressed = keyboardBitSet.get(KeyCode.D.ordinal());
+        boolean isLeftPressed = keyboardBitSet.get(KeyCode.LEFT.ordinal());
+        boolean isRightPressed = keyboardBitSet.get(KeyCode.RIGHT.ordinal());
+        boolean isSpacePressed = keyboardBitSet.get(KeyCode.SPACE.ordinal());
+
+            if (isLeftPressed && !isRightPressed) {
                 player.rotateLeft();
             }
 
-            else if (e.getCode() == KeyCode.RIGHT) {
+            else if ( !isLeftPressed && isRightPressed) {
                 player.rotateRight();
             }
 
-            else if (e.getCode() == KeyCode.A) {
+            if ( isAPressed && !isDPressed) {
                 enemy.rotateLeft();
             }
 
-            else if (e.getCode() == KeyCode.D) {
+            else if ( !isAPressed && isDPressed) {
                 enemy.rotateRight();
             }
 
-            else if (e.getCode() == KeyCode.SPACE) {
+            if ( isSpacePressed) {
                 Bullet bullet = new Bullet();
                 Bullet bullet2 = new Bullet();
                 // Setter bullet velocity til 5 ganger så mye som player
@@ -148,12 +148,25 @@ public class TankMain extends Application {
 
 
             }
-        });
 
+        for (GameObjects bullet : bullets) {
+            if (bullet.isColliding(enemy)) {
+                bullet.setAlive(false);
+                enemy.setAlive(false);
 
+                //remover bullet og enemy når de blir hit av bullet
 
+            }
 
-        stage.show();
+        }
+
+        bullets.removeIf(GameObjects::isDead);
+
+        bullets.forEach(GameObjects::update);
+
+        player.update();
+        enemy.update();
+        //Slett denne for å fjerne Røde dotter
 
     }
 
