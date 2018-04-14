838a670834a55c94dadb5f8aba6d37c420ba748e
Elias Larsen <larsenlzm@Eliass-MacBook-Air.local>
2018-04-15 00:50:33 +0200

    fix

--- src/TankMain.java
+++ src/TankMain.java
@@ -1,20 +1,8 @@
-import javafx.animation.AnimationTimer;
 import javafx.application.Application;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.geometry.Point2D;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.input.KeyEvent;
-import javafx.scene.layout.Pane;
-import javafx.scene.paint.Color;
 import javafx.stage.Stage;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.BitSet;
-import java.util.List;
 
 public class TankMain extends Application {
 
