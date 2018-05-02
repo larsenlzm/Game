e1d57672105b51ec5aed23bd23d34f5190c99aa6
Arsalan <ArsalanKhalid98@outlook.com>
2018-05-02 10:29:13 +0200

    fjerneUnodvendigKode

--- src/TankMain.java
+++ src/TankMain.java
@@ -13,12 +13,6 @@ public class TankMain extends Application {
     public void start(Stage mainStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("fuxml.fxml"));
         Scene scene = new Scene(root);
-        /* Kode for fullskjerm
-        Rectangle2D primaryScreenBounds = Screen.getPrimary().getVisualBounds();
-        mainStage.setX(primaryScreenBounds.getMinX());
-        mainStage.setY(primaryScreenBounds.getMinY());
-        mainStage.setWidth(primaryScreenBounds.getWidth());
-        mainStage.setHeight(primaryScreenBounds.getHeight()); */
         scene.getStylesheets().add("Style.css");
         mainStage.setTitle("TankSpillet");
         mainStage.setScene(scene);
