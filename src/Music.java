89bb62d2e5c2f17d0b56d5bd8d2410ad36e74d4f
Arsalan <ArsalanKhalid98@outlook.com>
2018-05-11 15:42:17 +0200

    back

--- src/Music.java
+++ src/Music.java
@@ -35,9 +35,8 @@ public class Music {
     }
 
     public void stop() {
-        if (clip == null) {
-            clip.stop();
-        }
+        if (clip == null) return;
+        clip.stop();
     }
 
     public void loop() {
