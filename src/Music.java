ded5508b3e6c766e5185d0e4b4e72deda8b736b4
Arsalan <ArsalanKhalid98@outlook.com>
2018-05-11 13:47:21 +0200

    backgroundMusic

---
+++ src/Music.java
@@ -0,0 +1,63 @@
+import javax.sound.sampled.*;
+
+public class Music {
+
+    private Clip clip;
+
+    public static Music sound1 = new Music("/res/backgroundMusic.wav");
+
+    public Music(String fileName) {
+        try {
+            AudioInputStream ais = AudioSystem.getAudioInputStream(Music.class.getResource(fileName));
+            clip = AudioSystem.getClip();
+            clip.open(ais);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void play() {
+        try {
+            if (clip != null) {
+                new Thread() {
+                    public void run() {
+                        synchronized (clip) {
+                            clip.stop();
+                            clip.setFramePosition(0);
+                            clip.start();
+                        }
+                    }
+                }.start();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void stop() {
+        if (clip == null) return;
+        clip.stop();
+    }
+
+    public void loop() {
+        try {
+            if (clip != null) {
+                new Thread() {
+                    public void run() {
+                        synchronized (clip) {
+                            clip.stop();
+                            clip.setFramePosition(0);
+                            clip.loop(Clip.LOOP_CONTINUOUSLY);
+                        }
+                    }
+                }.start();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public boolean isActive() {
+        return clip.isActive();
+    }
+}
